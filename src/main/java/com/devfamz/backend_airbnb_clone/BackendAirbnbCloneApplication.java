994b4a508dd40996f141aa04dc308f0ef2e9b31e
Zeeshan Rasheed <zeeshan.rasheed.goraya@gmail.com>
2024-07-27 20:33:20 +0500

    Initial Commit

---
+++ src/main/java/com/devfamz/backend_airbnb_clone/BackendAirbnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.devfamz.backend_airbnb_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAirbnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAirbnbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devfamz/backend_airbnb_clone/BackendAirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfamz.backend_airbnb_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
